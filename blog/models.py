5e240626d095bbe91d6642cb78bf26c388775016
Gunhot <lkh0107@sogang.ac.kr>
2023-06-25 14:43:26 +0900

    after models.py serializer.py

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
+class Subject(models.Model):
     name = models.CharField(max_length=30)
-    school = models.CharField(max_length=30)
+    year = models.IntegerField()
+    professor = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
-    
 
-class Blog(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog')
-    
+    score = models.IntegerField()
+    upload_time = models.DateTimeField(auto_now_add=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    attachment = models.FileField(upload_to='attachments/')
+
     def __str__(self):
         return self.title
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    subjects = models.ManyToManyField(Subject)
+    uploaded_posts = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True, blank=True, related_name='uploader')
+    downloaded_posts = models.ManyToManyField(Post, related_name='downloaders')
+
+    def __str__(self):
+        return self.name
--- blog/serializers.py
+++ blog/serializers.py
@@ -1,12 +1,25 @@
-from rest_framework.serializers import ModelSerializer
-from .models import User, Blog
+from rest_framework import serializers
+from .models import Subject, Post, User
 
-class UserSerializer(ModelSerializer):
+class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = Subject
         fields = '__all__'
 
-class BlogSerializer(ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    subject = SubjectSerializer()
+    uploader = serializers.StringRelatedField()
+    downloaders = serializers.StringRelatedField(many=True)
+
     class Meta:
-        model = Blog
+        model = Post
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    subjects = SubjectSerializer(many=True)
+    uploaded_posts = PostSerializer()
+    downloaded_posts = PostSerializer(many=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
